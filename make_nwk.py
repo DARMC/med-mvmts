f09b8e6d76ba6ff2dae72d5b12859fde3702bf5e
Brendan Maione-Downing <b.maionedowning@gmail.com>
2014-01-18 14:01:42 -0500

    Cleaned up structure

--- make_nwk.py
+++ make_nwk.py
@@ -2,31 +2,47 @@ import networkx as nx
 import unicodecsv as csv
 import matplotlib.pyplot as plt 
 
-with open('movements.csv','rU') as inf:
-	segments = [x for x in csv.reader(inf)][1:]
+def load_edge_table(infile):
+	"""
+	Returns edge table file intact except for header row.
+	"""
+	return [x for x in csv.reader(open(infile, 'rU'))][1:]
 
-with open('trips.csv', 'rU') as inf:
-	locations = [x[4] for x in csv.reader(inf)][1:]
+def load_points(infile):
+	"""
+	Returns only the name string of each point.
+	 * A point is unique iff its name does not match any other point.
+	"""
+	return [x[4] for x in csv.reader(open(infile, 'rU'))]
 
-candidates = list(set(locations))
+def make_node_table(all_points):
+	"""
+	Returns a dict with pairs (key, val) where:
+	 * Key: name string of the node
+	 * Val: unique integer ID of node
+	"""
+	dict = {}
+	for x in xrange(len(all_points)):
+		dict[all_points[x]] = x # assign UID to node
+	return dict
 
-location_dict = {}
+if __name__ == '__main__':
+	# make edge and node tables
+	print '>> Loading edge and node tables...',
+	edges = load_edge_table('movements.csv')
+	nodes = make_node_table(list(set(load_points('trips.csv'))))
+	print 'SUCCEEDED'
 
-for x in xrange(len(candidates)):
-	location_dict[candidates[x]] = x
+	# match start and end nodes to edges
+	print '>> Matching edges and nodes...',
+	for edge in edges:
+		edge.append(nodes[edge[2]])
+		edge.append(nodes[edge[5]])
+	print 'SUCCEEDED'
 
-
-#print location_dict
-for segment in segments:
-	segment.append(location_dict[segment[2]])
-	segment.append(location_dict[segment[5]])
-	print segment
-
-# segments now has to-from information
-G = nx.Graph()
-for s in segments:
-	G.add_edge(s[-2], s[-1])
-
-#nx.draw(G)
-print nx.betweenness_centrality(G)
-
+	# draw graph
+	print '>> Drawing graph...',
+	G = nx.Graph()
+	for e in edges:
+		G.add_edge(e[-2], e[-1])
+	print 'COMPLETE'
